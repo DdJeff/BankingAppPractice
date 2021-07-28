ca021fc8d2aa7d48d5116380fa37b0dac3bb4894
DEONTAY JEFFERSON <djefferson46@gmail.com>
2021-07-28 09:58:26 -0700

    itnitial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double deposit)
+        {
+            balance = deposit;
+        }
+
+        public double GetBalence()
+        {
+            return balance;
+        }
+
+        public double Withdrawal(double withdrawal)
+        {
+                balance = balance - withdrawal; //taking out monies
+
+                return balance;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new BankAccount();
+            {
+                Console.WriteLine(myAccount.GetBalence());
+                myAccount.Deposit(100.00);
+
+                Console.WriteLine();
+                Console.WriteLine(myAccount.GetBalence());
+
+                Console.WriteLine();
+                myAccount.Withdrawal(50.00);
+
+                Console.WriteLine();
+                Console.WriteLine(myAccount.GetBalence());
+
+            }
+        }
+    }
+}
